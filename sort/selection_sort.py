5cd4ab41b41634fe6ab97b6356c512d140a05c65
Erik Kvale <eirikval@gmail.com>
2018-06-10 15:50:00 -0500

    Working on selection sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,19 @@
+def find_smallest(_list):
+    """
+    Finds the smallest integer in an array
+
+    >>> demo_list = [4, 5 , 2, 6]
+    >>> find_smallest(demo_list)
+    2
+    """
+    smallest = _list[0]
+    for idx, num in enumerate(_list):
+        if num < smallest:
+            smallest = num
+            smallest_idx = idx
+    return smallest
+
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
